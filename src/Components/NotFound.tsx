e83426cbbb272e8c1c1f2f033c5e752a0225b029
Kyle Berkley <berkyle@gmail.com>
2020-01-23 22:51:26 -0600

    ✨ Improve 404 page.

--- src/Components/NotFound.tsx
+++ src/Components/NotFound.tsx
@@ -1,21 +1,38 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 const NotFound: React.FC = () => {
   useEffect(() => {
     document.title = "Resident 51 | 404";
   }, []);
 
+  const history = useHistory();
+
   return (
     <Container>
-      <Row>
-        <Col className="justify-contents-center">
-          <h1>Error: Page not found!</h1>
-          <Link to="/">Go Home</Link>
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} className="justify-contents-center">
+          <h1 className="my-4 text-center">Bad News Bears:</h1>
+          <p className="my-5 text-center lead">
+            There is nothing here. Are you happy? Is this what you wanted? *Ruffles your hair* Gah,
+            I can't stay mad at you :^)
+          </p>
+          <Button
+            className="text-center"
+            block
+            onClick={(): void => {
+              history.push("/");
+            }}
+            variant="primary"
+            type="submit"
+          >
+            Back to Civilization
+          </Button>
         </Col>
       </Row>
     </Container>
