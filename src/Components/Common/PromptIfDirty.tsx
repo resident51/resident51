061bd9601c404f0969d7628392a04b5929d12f80
Kyle Berkley <berkyle@gmail.com>
2020-01-25 20:39:52 -0600

    🐛 Stop navigation after invalid submit.

--- src/Components/Common/PromptIfDirty.tsx
+++ src/Components/Common/PromptIfDirty.tsx
@@ -7,7 +7,7 @@ const PromptIfDirty: React.FC = () => {
   const formik = useFormikContext();
   return (
     <Prompt
-      when={formik.dirty && formik.submitCount === 0}
+      when={formik.dirty || formik.submitCount > 0}
       message="Are you sure you want to leave? You have unsaved changes."
     />
   );
