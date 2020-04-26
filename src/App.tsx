89dd6f8325c03fe57fbb6a536e24cb58c9292152
Kyle Berkley <berkyle@gmail.com>
2020-04-26 16:24:07 -0500

    Remove unused bootstrap-y components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-// import Header from './Components/Layout/Header';
 import Routes from './Components/Routes';
-
-// import 'react-dates/lib/css/_datepicker.css';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './Styles/App.scss';
-// // import "./Styles/R51-Title.scss";
 
 import { EventsProvider } from './Contexts/Events';
 import { UserProvider } from './Contexts/User';
