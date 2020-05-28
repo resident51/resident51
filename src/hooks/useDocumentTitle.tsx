767c984514976ac7e23d03979a79c15e096ed83f
Kyle Berkley <berkyle@gmail.com>
2020-05-27 20:39:27 -0500

    renaming attempt part 1/2

--- src/hooks/useDocumentTitle.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-const useDocumentTitle = (title: string): void => {
-  useEffect(() => {
-    document.title = `Resident 51 | ${title}`;
-  }, [title]);
-};
-
-export default useDocumentTitle;
